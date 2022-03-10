7454b9fc7d7e36a286283e246499553bca01207e
Nairnbru007 <31794657+Nairnbru007@users.noreply.github.com>
2022-03-11 01:59:52 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,9 @@
-from main.py import start_work
-file='...'
+import sys
+
+try:
+    file = sys.argv[1] # получаем 1й элемент списка (в примерах выше именно он отвечает за имя файла)
+except IndexError: 
+    sys.exit("error! choose the file!") 
+
+from main import start_work
 start_work(file,noise_reduce=False,cutting=False)
