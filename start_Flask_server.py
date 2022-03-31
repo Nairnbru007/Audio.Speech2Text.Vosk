dc490aa66075d5898f0c45e14af9575aa50cb09a
Nairnbru007 <31794657+Nairnbru007@users.noreply.github.com>
2022-03-31 19:45:21 +0300

    Update start_Flask_server.py

--- start_Flask_server.py
+++ start_Flask_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, flash
+from flask import Flask, request, flash, render_template, make_response, Response
 from flask_cors import CORS, cross_origin
 from flask_restx import Api, Resource, fields, marshal, reqparse
 import werkzeug
@@ -25,17 +25,29 @@ file_upload.add_argument('file',
                           help='Image file cannot empty')
 
 
-@namespace.route('/')
+@api.route('/',methods = ['GET'])
+class UP(Resource):
+   def __init__(self):
+      pass
+   def get(self):
+      headers = {'Content-Type': 'text/html'}
+      return Response(render_template('index.html'),200,headers)
+
+@namespace.route('/123')
 class DetectApi(Resource):
     @namespace.expect(file_upload)
     def post(self):
         args = file_upload.parse_args()
         file_ = args['file']
         results = 'i send it'
-        return {'results': results, 'error': ''}, 200				  
-						  
+        return {'results': results, 'error': ''}, 200
+ 
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('index.html'),200,headers)
+ 
 if __name__ == '__main__':
-    app.run(port=5000, debug=False, host='127.0.0.1')  # , host='0.0.0.0'					  
+    app.run(port=5001, debug=False, host='0.0.0.0')  # , host='0.0.0.0'				  
 						  
 						  
 					  
