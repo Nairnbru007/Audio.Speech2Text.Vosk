4136bb1ce6cff8639028ec4e060a958f327f4103
Nairnbru007 <31794657+Nairnbru007@users.noreply.github.com>
2022-04-03 20:08:22 +0300

    Update start_Flask_server.py

--- start_Flask_server.py
+++ start_Flask_server.py
@@ -4,6 +4,11 @@ from flask_restx import Api, Resource, fields, marshal, reqparse
 import werkzeug
 import sys
 import logging
+import time
+import os
+from werkzeug.utils import secure_filename
+from main_server import start_work
+#from utils import make_salted_hash, check_hashed_password, allowed_file, get_save_name, get_md5, save_uploadfile_to_backup
 
 app = Flask('edges')
 
@@ -21,7 +26,7 @@ file_upload = reqparse.RequestParser()
 file_upload.add_argument('file',
                           type=werkzeug.datastructures.FileStorage,
                           location='files',
-                          required=True,
+                          required=False,
                           help='Image file cannot empty')
 
 
@@ -33,21 +38,38 @@ class UP(Resource):
       headers = {'Content-Type': 'text/html'}
       return Response(render_template('index.html'),200,headers)
 
-@namespace.route('/123')
+def allowed_files(file):
+        if file.split('.')[-1].lower() in ['txt','avi','mp4','mp3','wav']:
+                return True
+
+@namespace.route('/mediafile')
 class DetectApi(Resource):
-    @namespace.expect(file_upload)
-    def post(self):
+   @namespace.expect(file_upload)
+   def post(self):
         args = file_upload.parse_args()
-        file_ = args['file']
-        results = 'i send it'
-        return {'results': results, 'error': ''}, 200
- 
-    def get(self):
-        headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('index.html'),200,headers)
- 
+        file = args['file']
+        result = 'server get the file'
+        if file and allowed_files(file.filename):
+                filename = secure_filename(file.filename)
+                path = time.strftime("%Y_%m_%d-%H_%M_%S")
+                if not os.path.exists(path):
+                   os.makedirs(path)
+                file.save(os.path.join(path, filename))
+                start_work(os.path.join(path, filename),noise_reduce=False,cutting=False)
+                for i in os.listdir(path):
+                   if i.endswith(".txt"):
+                      out_txt = os.path.join(path, i)
+                      with open(out_txt,'r') as fi:
+                         result={'text':fi.read()}
+                         print(result)
+                         return {'results': result, 'error': ''}, 200 
+                return {'results': result, 'error': ''}, 200 
+        else:
+                return {'results': '', 'error': 'file not allowed'}, 200 
+        #return {'results': results, 'error': ''}, 200 
 if __name__ == '__main__':
-    app.run(port=5001, debug=False, host='0.0.0.0')  # , host='0.0.0.0'				  
+    app.run(port=5001, debug=False, host='0.0.0.0')  # , host='0.0.0.0'
+
 						  
 						  
 					  
