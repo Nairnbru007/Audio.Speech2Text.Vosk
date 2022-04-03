3d2aaf6e2758f3b203719493c89a4758af8d4203
Nairnbru007 <31794657+Nairnbru007@users.noreply.github.com>
2022-04-03 16:30:23 +0300

    Update script_post_file_to_Flask.py

--- script_post_file_to_Flask.py
+++ script_post_file_to_Flask.py
@@ -2,7 +2,7 @@ import sys
 import requests
 file_=sys.argv[1]
 with open(file_, 'rb') as f:
-    r = requests.post('http://127.0.0.1:5001/api/123', files={'file': f})
+    r = requests.post('http://127.0.0.1:5001/api/mediafile', files={'file': f})
 print(r.text)
 
 
