ccbaef045570971862522df789303bd6277bf173
Nairnbru007 <31794657+Nairnbru007@users.noreply.github.com>
2022-05-03 09:19:13 +0300

    Update update_model.py

--- update_model.py
+++ update_model.py
@@ -4,27 +4,27 @@ import os
 
 import time
 
-
-local_dir='model_new'
-if not os.path.exists(local_dir):
-    os.makedirs(local_dir)
-local_dir='model_new/exp/chain/tdnn/graph'
-if not os.path.exists(local_dir):
-    os.makedirs(local_dir)
-local_dir='model_new/rescore'
-if not os.path.exists(local_dir):
-    os.makedirs(local_dir)
-local_dir='model_new/rnnlm'
-if not os.path.exists(local_dir):
-    os.makedirs(local_dir)
+local_dir_graph='/opt/vosk-model-en/model/graph/'
+local_dir_rescore='/opt/vosk-model-en/model/rescore/'
+local_dir_rnnlm='/opt/vosk-model-en/model/rnnlm/'
     
-# graph
+#local_dir_graph='model_new/exp/chain/tdnn/graph'
+if not os.path.exists(local_dir_graph):
+    os.makedirs(local_dir_graph)
+#local_dir='model_new/rescore'
+if not os.path.exists(local_dir_rescore):
+    os.makedirs(local_dir_rescore) 
+#local_dir='model_new/rnnlm'
+if not os.path.exists(local_dir_rnnlm):
+    os.makedirs(local_dir_rnnlm)
+    
+# graph path in beget
 # exp/chain/tdnn/graph
 
-# rescore
+# rescore path in beget
 # data/lang_test_rescore/G.fst
 
-# rnnlm
+# rnnlm path in beget
 # exp/rnnlm_out
 
 def getting_(sftp,remote_path, local_path, file='__all__'):
@@ -54,9 +54,9 @@ with pysftp.Connection(
     password = my_Password 
     ) as sftp: 
     print("Connection succesfully established ... ") 
-    getting_(sftp,'exp/chain/tdnn/graph/', 'model_new/exp/chain/tdnn/graph/')
-    getting_(sftp,'data/lang_test_rescore/', 'model_new/rescore/', 'G.fst')
-    getting_(sftp,'exp/rnnlm_out/', 'model_new/rnnlm/')
+    getting_(sftp,'exp/chain/tdnn/graph/', local_dir_graph)
+    getting_(sftp,'data/lang_test_rescore/', local_dir_rescore, 'G.fst')
+    getting_(sftp,'exp/rnnlm_out/', local_dir_rnnlm)
 print('DONE')
          
 
